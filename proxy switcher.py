60108e651234569e5bf433504e6900c4b2eb397f
devtill2yeah <devtill2yeah@yahoo.com>
2022-12-16 02:52:51 +0100

    Update proxy switcher.py

--- proxy switcher.py
+++ proxy switcher.py
@@ -1,5 +1,6 @@
 
 
+
 #we need to import a few things to make it work first
 from lxml.html import fromstring
 from itertools import cycle
@@ -34,4 +35,5 @@ for i in range(0,len(PROXY_LIST)):
 
 #lists all working proxies, it converts the list to a set so it only shows unique values.
 print("The working proxies are:"+ str(set(working_proxies)))
+
 
